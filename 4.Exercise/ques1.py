09cd4b3134ce1e18d6f3acef6d76870f9bac9fa4
Anubhav Tandon <41839606+anubhav-tandon@users.noreply.github.com>
2019-01-04 22:11:41 +0530

    Add files via upload

---
+++ 4.Exercise/ques1.py
@@ -0,0 +1,7 @@
+x=input("Enter value of x:")
+y=input("Enter value of y:")
+print "Left shift  x:",x<<2,"\nRight shift y:",y>>2,"\nx and y bitwise:",x&y,"\nx or y bitwise:",x|y
+if(x<10 and y<10):
+    print"Logical Operator applied!"
+    
+print"x to power y is: ",x**y 
