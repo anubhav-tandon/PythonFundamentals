08601500411c358594acd02fd3d6c2b12ecc7df7
Anubhav Tandon <41839606+anubhav-tandon@users.noreply.github.com>
2019-01-04 22:12:34 +0530

    Add files via upload

---
+++ 4.Exercise/ques3.py
@@ -0,0 +1,13 @@
+st="abcd"
+st2="xyz"
+list1=['abc',96,'xy',965]
+list2=['pqr',100,'wx',5]
+tup=('hello','world')
+tup2=('tuple','two')
+dic={"H":"Python","K":200}
+dic2={"I":"Java","J":400}
+
+print st+st2," ",list1+list2," ",tup+tup2," ",dic["K"]+dic2["J"]
+list1.append(50)
+print list1
+
